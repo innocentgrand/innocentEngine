ba8ada0cc07b28ffd007ae962a32758dd1336f76
yamakawa <yamakawa@www13333uj.sakura.ne.jp>
2015-02-02 21:29:28 +0900

    XFileを大改造

--- innocent/core/Xfiles.php
+++ innocent/core/Xfiles.php
@@ -31,9 +31,6 @@ try{
 
 	$tmpAlias = $x_config->getAlias();
 
-	if(!empty($tmpAlias)){
-		pr($tmpAlias);
-	}
 
 	if ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ){
 			$protocol = "https://";
@@ -45,56 +42,67 @@ try{
 
 	$x_parse_url = parse_url($x_request);
 
-	$x_exp_path = explode('/',$x_parse_url["path"]);
-	
-	if($x_exp_path[1] == ""){
-			$x_class_sub_name = "Default";
-	} 
-	else {
-			$x_class_sub_name = ucfirst($x_exp_path[1]);
-	}
-	
-	$x_class_name = $x_class_sub_name."Controller";
-	
-	$x_controller_file = $x_path_controller . $x_class_name . ".php";
-
-	if(!file_exists($x_controller_file)){
-		throw new Exception("file not exists");
-	}
-
-	require_once($x_controller_file);
-
-	if(!class_exists($x_class_name)){
-		throw new Exception("class not exists");
-	}
-
-	$x_object = new $x_class_name();
-	$x_object->tplPathsetter($x_path_view.$x_class_sub_name);
-	$x_object->logDirSetter($x_path_log);
-	$x_object->modelPathSetter($x_path_modl);
-	$x_object->dbDataSetter($x_config->defaulDbGetter());
-
-	$x_object->startUp();
-
-	if(empty($x_exp_path[2])){
-		$x_method = "Index";
-	} 
-	else {
-		$x_mehod = ucfirst($x_exp_path[2]);
-	}
-
-	if(!method_exists($x_object,$x_method)){
-			$text = <<<TEXT
-{$x_class_name} not method exists
-TEXT;
-		throw new Exception($text);
-	}
-
-	$x_object->$x_method();
-
-} catch (Exception $ex){
+	xfileSecend(&$x_config,&$x_path_controller,&$x_parse_url);	
+}catch (Exception $ex){
 	echo nl2br($ex->getMessage());
 }
+
+
+function xfileSecend($x_config,$x_parse_url,$x_path_controller,$parseCount=1){
+	try{
+		$x_exp_path = explode('/',$x_parse_url["path"]);
+	
+		if($x_exp_path[$parseCount] == ""){
+				$x_class_sub_name = "Default";
+		} 
+		else {
+			$x_class_sub_name = ucfirst($x_exp_path[1]);
+		}
+
+		$x_class_name = $x_class_sub_name."Controller";
+
+		$x_controller_file = $x_path_controller . $x_class_name . ".php";
+	
+		if(!file_exists($x_controller_file)){
+			throw new Exception("file not exists");
+		}
+
+		require_once($x_controller_file);
+
+		if(!class_exists($x_class_name)){
+			throw new Exception("class not exists");
+		}
+
+		$x_object = new $x_class_name();
+		$x_object->tplPathsetter($x_path_view.$x_class_sub_name);
+		$x_object->logDirSetter($x_path_log);
+		$x_object->modelPathSetter($x_path_modl);
+		$x_object->dbDataSetter($x_config->defaulDbGetter());
+
+		$x_object->startUp();
+
+		if(empty($x_exp_path[2])){
+			$x_method = "Index";
+		} 
+		else {
+			$x_mehod = ucfirst($x_exp_path[2]);
+		}
+
+		if(!method_exists($x_object,$x_method)){
+				$text = <<<TEXT
+{$x_class_name} not method exists
+TEXT;
+			throw new Exception($text);
+		}
+
+		$x_object->$x_method();
+
+	} catch (Exception $ex){
+		throw($ex);
+	}
+		pr($configObj);
+}
+
 function pr($data){
 		echo "<pre>";
 		var_dump($data);
